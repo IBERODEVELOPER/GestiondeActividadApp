52d13440eeac9623bf76244758d54501b1ca4e89
raul1995 <tamani1995@gmail.com>
2021-01-08 19:00:45 -0500

    RAUL TAMANI : APLICACION DE GESTION DE ACTIVIDADES  COMIT

---
+++ app/src/main/java/informatico/to/gestiondeactividadapp/MainActivity.java
@@ -0,0 +1,18 @@
+package informatico.to.gestiondeactividadapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //para el icono en el action bar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //llamamo a la imagen por su nombre que tiene que estar en drawable
+        getSupportActionBar().setIcon(R.drawable.ic_action_name);
+    }
+}
